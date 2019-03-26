cd0a4a0565c3ace171d277515aaeadcc116f5051
Yichun Sung <cd12631@gmail.com>
2019-03-26 12:18:37 +0800

    add Chinese name

---
+++ runCode1To200_C.R
@@ -0,0 +1,17 @@
+# 1到200筆
+
+source('downloadCWBData.R')
+
+library(data.table)
+library(readxl)
+
+stationList <- read_xlsx("data/new_Station_List.xlsx", sheet = "new_Station_List")
+
+date <- Sys.Date()-2
+
+
+for(i in 1:200){
+  svePath <- paste("writeData/", date, "-", as.vector(stationList$Name[i]), ".csv",  sep = "")
+  download_cwb_data_eng(as.vector(stationList$engName[i]), date, date, svePath)
+  
+}
